350bd78b36b01b21b4c6662f617016651f72c3c1
David Laprade <david.laprade@gmail.com>
2018-02-21 10:27:39 -0500

    Define an engine-level root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CangarooUI::Engine.routes.draw do
+  root to: "transactions#index"
   resources :transactions, only: [:index, :show]
   resources :records, only: [:index, :show, :update]
   resources :retry_jobs, only: [:update]
