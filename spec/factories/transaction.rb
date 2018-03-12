e87c7c02edc9477db4f98ab25393edfe0a3146b6
David Laprade <david.laprade@gmail.com>
2018-03-12 11:16:26 -0400

    FIXUP Persist the unique ActiveJob ID to

--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     source_connection      factory: :ui_connection
     record
     job
-
+    active_job_id "9ac8d967-df9c-44fe-b91f-3fc3d6adbff3"
     job_class "MyFake::Job"
   end
 
