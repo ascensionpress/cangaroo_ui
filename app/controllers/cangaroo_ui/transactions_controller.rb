67f42d7e56d8372f7af6136759359c623274c1c7
David Laprade <david.laprade@gmail.com>
2018-07-17 12:22:08 -0400

    Allow users to filter transaction indexes by job class

--- app/controllers/cangaroo_ui/transactions_controller.rb
+++ app/controllers/cangaroo_ui/transactions_controller.rb
@@ -2,12 +2,17 @@ module CangarooUI
   class TransactionsController < ApplicationController
 
     def index
+      # if this logic gets any more complex, abstract into a search service
       @push_txs = CangarooUI::Transaction.order(id: :desc)
         .where(job_class: Rails.configuration.cangaroo.jobs.map(&:name))
-        .paginate(page: params[:page])
-      @poll_txs = Rails.configuration.cangaroo.poll_jobs.map do |poll_job|
-        CangarooUI::Transaction.where(job_class: poll_job.name).order(last_run: :desc).first
+      if @job_class = params[:job_class].presence
+        @push_txs = @push_txs.where(job_class: @job_class)
+      else
+        @poll_txs = Rails.configuration.cangaroo.poll_jobs.map do |poll_job|
+          CangarooUI::Transaction.where(job_class: poll_job.name).order(last_run: :desc).first
+        end
       end
+      @push_txs = @push_txs.paginate(page: params[:page])
     end
 
     def show
